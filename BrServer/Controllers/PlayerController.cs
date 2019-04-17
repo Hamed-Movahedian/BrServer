987354bd64f9e01c968971ec5465c3ba3dc10d4f
DESKTOP-SM4SNDC\Hamed <Movahedian.h@gmail.com>
2019-04-17 10:00:24 +0430

    no message

--- BrServer/Controllers/PlayerController.cs
+++ BrServer/Controllers/PlayerController.cs
@@ -20,6 +20,100 @@ namespace BrServer.Controllers
     {
         private charsoog_BrDBEntities db = new charsoog_BrDBEntities();
 
+
+        [HttpPost, Route("Save")]
+        public IHttpActionResult Save([FromBody] string input)
+        {
+            var jInput = JToken.Parse(input);
+
+            var jStat = jInput["PlayerStat"];
+
+            var player = new Player
+            {
+                Id = jInput["ID"].Value<int>(),
+                Name = jInput["Name"].Value<string>(),
+                AiBehaviorIndex = jInput["AiBehaviorIndex"].Value<short>(),
+                CoinCount = jInput["CoinCount"].Value<int>(),
+                TicketCount = jInput["TicketCount"].Value<int>(),
+                HasBattlePass = jInput["HasBattlePass"].Value<bool>(),
+
+                CurrentCharacter = jInput["CurrentCharacter"].Value<int>(),
+                CurrentFlag = jInput["CurrentFlag"].Value<int>(),
+                CurrentEmote = jInput["CurrentEmote"].Value<int>(),
+                CurrentPara = jInput["CurrentPara"].Value<int>(),
+
+                TotalBattles= jStat["TotalBattles"].Value<int>(),
+                TotalWins= jStat["TotalWins"].Value<int>(),
+                TotalKills= jStat["TotalKills"].Value<int>(),
+                DoubleKills= jStat["DoubleKills"].Value<int>(),
+                TripleKills = jStat["TripleKills"].Value<int>(),
+                ItemsCollected = jStat["ItemsCollected"].Value<int>(),
+                GunsCollected = jStat["GunsCollected"].Value<int>(),
+                SupplyDrop = jStat["SupplyDrop"].Value<int>(),
+                SupplyCreates = jStat["SupplyCreates"].Value<int>(),
+                Experience = jStat["Experience"].Value<int>(),
+
+            };
+
+            // add characters
+            foreach (var i in jInput["AvalableCharacters"].Values<int>())
+            {
+                var character = db.Characters.Find(i);
+
+                if (character == null)
+                    return BadRequest($"Character id={i} not found.");
+
+                player.Characters.Add(character);
+            };
+
+            // add Flags
+            foreach (var i in jInput["AvalableFlags"].Values<int>())
+            {
+                var flag = db.Flags.Find(i);
+
+                if (flag == null)
+                    return BadRequest($"Flag id={i} not found.");
+
+                player.Flags.Add(flag);
+            };
+
+            // add Emotes
+            foreach (var i in jInput["AvalableEmotes"].Values<int>())
+            {
+                var emote = db.Emotes.Find(i);
+
+                if (emote == null)
+                    return BadRequest($"Emote id={i} not found.");
+
+                player.Emotes.Add(emote);
+            };
+
+            // add paras
+            foreach (var i in jInput["AvalableParas"].Values<int>())
+            {
+                var para = db.Parachutes.Find(i);
+
+                if (para == null)
+                    return BadRequest($"Para id={i} not found.");
+
+                player.Parachutes.Add(para);
+            };
+
+            db.Players.Attach(player);
+            db.Entry(player).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+
+            return Ok("OK");
+        }
+
         [HttpPost, Route("GetInitialInfo")]
         public IHttpActionResult GetInitialInfo([FromBody] string input)
         {
@@ -33,7 +127,7 @@ namespace BrServer.Controllers
                 player= db.Players.Find(id);
             else
             {
-                // get avalible characters, flags, paras, emotes
+                // get available characters, flags, paras, emotes
                 var characters = db.Characters.Where(c => c.HasByDefault).ToList();
                 var flags = db.Flags.Where(c => c.HasByDefault).ToList();
                 var emotes = db.Emotes.Where(c => c.HasByDefault).ToList();
